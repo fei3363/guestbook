20519a4e4ec52f65c6d3b4cd3b28a4c2d2ed36cd
飛飛 <82772249+fei3363@users.noreply.github.com>
2023-04-29 13:56:41 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -3,6 +3,48 @@
 // 輸入帳號跟密碼+註冊按鈕 --> HTML 實作
 // 跟資料庫互動 --> PHP 實作
 
+// 測試資料庫是否有正常連線
+// 1. 宣告變數
+// 2. 連線到資料庫
+// 3. 確認連線狀態
+
+// 宣告變數
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', 'testtest');
+define('DB_NAME', 'mydb');
+
+// 進行連線
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// 確認狀態
+if ($link === false) {
+  die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+// 接受表單資料，確認請求的方法是否為 POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // 取得表單資料
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  // 寫入資料庫
+  // 1. 準備 SQL 語法
+  // 2. 執行
+  // 3. 確認是否寫入成功
+
+  // 準備 SQL 語法
+  $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+  // 執行
+  if (mysqli_query($link, $sql)) {
+    echo "註冊成功";
+  } else {
+    echo "註冊失敗";
+  }
+}
+
+
 ?>
 
 <html>
