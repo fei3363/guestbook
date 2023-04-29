eca61ea7d0e1a94785b7bd7f529ea68a84cee8c1
飛飛 <82772249+fei3363@users.noreply.github.com>
2023-04-29 14:48:49 +0800

    新增留言的功能，確認可以新增內容。

--- index.php
+++ index.php
@@ -10,19 +10,19 @@
 // // 2. 連線到資料庫
 // // 3. 確認連線狀態
 
-// // 宣告變數
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'user');
-// define('DB_PASSWORD', 'testtest');
-// define('DB_NAME', 'mydb');
+// 宣告變數
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', 'testtest');
+define('DB_NAME', 'mydb');
 
-// // 進行連線
-// $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+// 進行連線
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// // 確認狀態
-// if ($link === false) {
-//   die("ERROR: Could not connect. " . mysqli_connect_error());
-// }
+// 確認狀態
+if ($link === false) {
+  die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 // // 確認連線成功
 // echo "Connect Successfully. Host info: " . mysqli_get_host_info($link);
 
@@ -32,30 +32,57 @@ session_start();
 
 // 首頁，確認是否登入，如果沒有登入，跳轉到登入介面
 if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
-  // 跳轉到 login.php
-  header("Location: login.php");
-  exit;
+    // 跳轉到 login.php
+    header("Location: login.php");
+    exit;
 }
 
-// 構建新增留言
+// 構建新增留言，確認是否為 POST 請求，以及 content 是否有內容
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // 取得表單資料
+    $content = $_POST['content'];
+    // 確認 content 是否為空
+    if (empty($content)) {
+        echo "留言不得為空";
+    } else {
+        // 留言不為空，新增留言
+        $id = $_SESSION['id'];
+        // 1. 準備 SQL 語法
+        $sql = "INSERT INTO messages (user_id, message) VALUES ('$id', '$content')";
+        // 2. 執行
+        $result = mysqli_query($link, $sql);
+        // 3. 確認是否正確
+        if ($result) {
+            // 新增成功，跳轉到 index.php
+            header("Location: index.php");
+        } else {
+            // 新增失敗
+            echo "新增失敗";
+        }
+
+    }
+}
 
 ?>
 <!-- 新增留言介面 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>首頁</title>
-    </head>
-    <body>
-        <h1>歡迎 <?php echo $_SESSION['username']; ?></h1>
-        <b>新增留言</b>
-        <form method="POST" action="index.php">
+
+<head>
+    <meta charset="UTF-8">
+    <title>首頁</title>
+</head>
+
+<body>
+    <h1>歡迎
+        <?php echo $_SESSION['username']; ?>
+    </h1>
+    <b>新增留言</b>
+    <form method="POST" action="index.php">
         <!-- textarea -->
         <textarea name="content" placeholder="請輸入留言"></textarea>
-            <!-- <input type="text" name="content" placeholder="請輸入留言"> -->
-            <input type="submit" value="新增">
-        </form>
-    </body>
+        <!-- <input type="text" name="content" placeholder="請輸入留言"> -->
+        <input type="submit" value="新增">
+    </form>
+</body>
+
 </html>
-
-
